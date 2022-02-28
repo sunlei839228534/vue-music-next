af0ab8d033ef1655a8dbeaf6a4f331b1b6c5a2ce
孙磊 <839228534@qq.com>
2022-02-28 23:21:15 +0800

    feat: 歌手详情

--- src/views/singer-detail.vue
+++ src/views/singer-detail.vue
@@ -1,22 +1,47 @@
 <template>
-  <div class="singer-detail"></div>
+  <div class="singer-detail">
+    <music-list
+      :loading="loading"
+      :title="title"
+      :pic="pic"
+      :songs="songs"
+    ></music-list>
+  </div>
 </template>
 
 <script>
 import { getSingerDetailList } from "@/service/singer";
 import { getSongs } from "@/service/song";
+import MusicList from "@/components/music-list/music-list.vue";
 
 export default {
   name: "singer-detail",
+  components: {
+    MusicList,
+  },
   props: {
     singer: {
       type: Object,
     },
   },
+  data() {
+    return {
+      songs: [],
+      loading: true,
+    };
+  },
   async created() {
     const result = await getSingerDetailList(this.singer.mid);
-    const songs = await getSongs(result.songs);
-    console.log(songs);
+    this.songs = await getSongs(result.songs);
+    this.loading = false;
+  },
+  computed: {
+    pic() {
+      return this.singer && this.singer.pic;
+    },
+    title() {
+      return this.singer && this.singer.name;
+    },
   },
 };
 </script>
