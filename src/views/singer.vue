56490700b4178a1e8ead15ca221b10769ad2bea4
sunlei <839228534@qq.com>
2022-03-01 20:12:14 +0800

    feat: 增加过渡动画

--- src/views/singer.vue
+++ src/views/singer.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="singer" v-loading="!singers.length">
     <index-list @select="onSelectSinger" :data="singers"></index-list>
-    <router-view :singer="selectedSinger"></router-view>
+    <router-view v-slot="{ Component }">
+      <transition appear name="slide">
+        <component :is="Component"></component>
+      </transition>
+    </router-view>
   </div>
 </template>
 
